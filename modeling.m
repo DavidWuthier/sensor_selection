204f5637a74020133a0744ea2a4081562554e813
David Wuthier <daw@m-tech.aau.dk>
2019-06-24 11:31:07 +0200

    Fix modeling

--- modeling.m
+++ modeling.m
@@ -11,32 +11,37 @@ clc
 % h1 = matlabFunction(diff(t,ptx), 'File', 'h1');
 % h2 = matlabFunction(diff(t,pty), 'File', 'h2');
 
-ns = 8;
+ns = 40;
+ptx = 0.1;
+pty = 0.0;
 
 p = (2*pi)/ns*(0:ns-1);
 
-psx = (1 + p).*cos(p);
-psy = (1 + p).*sin(p);
+% psx = (1 + p).*cos(p);
+% psy = (1 + p).*sin(p);
+psx = cos(p);
+psy = sin(p);
+psx(1:10) = 0.5*psx(1:10);
+psy(1:10) = 0.5*psy(1:10);
 ts = p + pi;
 A = zeros(ns,2);
 a = zeros(2,2,ns);
 
 for k = 1:ns
-    A(k,1) = h1(psx(k), psy(k), 0, 0, ts(k));
-    A(k,2) = h2(psx(k), psy(k), 0, 0, ts(k));
+    A(k,1) = h1(psx(k), psy(k), ptx, pty, ts(k));
+    A(k,2) = h2(psx(k), psy(k), ptx, pty, ts(k));
     a(:,:,k) = A(k,:).' * A(k,:);
 end
-
-a11 = reshape(a(1,1,:),[1 ns]);
-a21 = reshape(a(2,1,:),[1 ns]);
-a12 = reshape(a(1,2,:),[1 ns]);
-a22 = reshape(a(2,2,:),[1 ns]);
 
 k = 4;
 
 cvx_begin
     variable z(ns)
-    maximize(log_det([a11*z a12*z ; a21*z a22*z]))
+    expression s(2, 2, ns)
+    for i = 1:ns
+        s(:,:,i) = z(i) * A(i,:).' * A(i,:);
+    end
+    maximize(log_det(sum(s, 3)))
     subject to
         ones(1,ns)*z == k
         0 <= z <= 1
@@ -44,5 +49,9 @@ cvx_end
 
 figure
 quiver(psx, psy, cos(ts), sin(ts), 0.2)
+hold on
 grid on
+axis square
+plot(ptx, pty, 'ro')
+quiver(psx, psy, z.'.*cos(ts), z.'.*sin(ts), 0.2, 'r')
 
