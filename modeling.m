2f38136b2cf1568c763f6c1060556c28f533b51a
David Wuthier <daw@m-tech.aau.dk>
2019-06-24 10:28:35 +0200

    Fix modeling

--- modeling.m
+++ modeling.m
@@ -3,20 +3,20 @@ close all
 clear
 clc
 
-syms ts ptx pty psx psy
-
-t = atan((-sin(ts)*(ptx - psx) + cos(ts)*(pty - psy))/...
-         (cos(ts)*(ptx - psx) + sin(ts)*(pty - psy)));
-
-h1 = matlabFunction(diff(t,ptx));
-h2 = matlabFunction(diff(t,pty));
+% syms ts ptx pty psx psy
+% 
+% t = atan((-sin(ts)*(ptx - psx) + cos(ts)*(pty - psy))/...
+%          (cos(ts)*(ptx - psx) + sin(ts)*(pty - psy)));
+% 
+% h1 = matlabFunction(diff(t,ptx), 'File', 'h1');
+% h2 = matlabFunction(diff(t,pty), 'File', 'h2');
 
 ns = 8;
 
 p = (2*pi)/ns*(0:ns-1);
 
-psx = cos(p);
-psy = sin(p);
+psx = (1 + p).*cos(p);
+psy = (1 + p).*sin(p);
 ts = p + pi;
 A = zeros(ns,2);
 a = zeros(2,2,ns);
@@ -36,8 +36,13 @@ k = 4;
 
 cvx_begin
     variable z(ns)
-    maximize((a11*z)*(a22*z) + (a21*z)*(a12*z))
+    maximize(log_det([a11*z a12*z ; a21*z a22*z]))
     subject to
         ones(1,ns)*z == k
         0 <= z <= 1
 cvx_end
+
+figure
+quiver(psx, psy, cos(ts), sin(ts), 0.2)
+grid on
+
